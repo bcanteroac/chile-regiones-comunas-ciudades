10c4ac9cb375bbe52c3b5eb38d0114c74f32db28
bcanteroac <91626533+bcanteroac@users.noreply.github.com>
2025-06-17 21:39:53 -0400

    vue 3 select, regiones, provincias y comunas

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,90 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <div class="container">
 
-  <main>
-    <TheWelcome />
-  </main>
+    <el-select v-model="selectedRegion" filterable placeholder="Selecciona una región" style="width: 240px">
+      <el-option v-for="item in regiones" :key="item.id" :label="item.nombre" :value="item.id" />
+    </el-select>
+
+    <el-select v-model="selectedProvincia" :disabled="!selectedRegion" filterable placeholder="Selecciona una ciudad"
+      style="width: 240px">
+      <el-option v-for="item in provinciasFiltradas" :key="item.id" :label="item.nombre" :value="item.id" />
+    </el-select>
+
+    <el-select v-model="selectedComuna" :disabled="!selectedProvincia" filterable placeholder="Selecciona una comuna"
+      style="width: 240px">
+      <el-option v-for="item in comunasFiltradas" :key="item.id" :label="item.nombre" :value="item.id" />
+    </el-select>
+
+  </div>
+
 </template>
 
+<script setup lang="ts">
+import { ref, computed, onMounted, watch } from 'vue';
+
+const regiones = ref([]);
+const provincias = ref([]);
+const comunas = ref([]);
+
+const selectedRegion = ref<number | null>(null);
+const selectedProvincia = ref<number | null>(null);
+const selectedComuna = ref<number | null>(null);
+
+const provinciasFiltradas = computed(() =>
+  provincias.value.filter(c => c.region_id === selectedRegion.value)
+);
+
+const comunasFiltradas = computed(() =>
+  comunas.value.filter(c => c.provincia_id === selectedProvincia.value)
+);
+
+// Reset de valores dependientes
+watch(selectedRegion, () => {
+  selectedProvincia.value = null;
+  selectedComuna.value = null;
+});
+
+watch(selectedProvincia, () => {
+  selectedComuna.value = null;
+});
+
+onMounted(() => {
+  fetch('/regiones.json')
+    .then(response => response.json())
+    .then(data => {
+      regiones.value = data.regiones;
+    });
+
+  fetch('/provincias.json')
+    .then(response => response.json())
+    .then(data => {
+      provincias.value = data.provincias;
+    });
+
+  fetch('/comunas.json')
+    .then(response => response.json())
+    .then(data => {
+      comunas.value = data.comunas;
+    });
+});
+</script>
+
+
 <style scoped>
-header {
-  line-height: 1.5;
+
+* {
+  /* border: 1px solid red; */
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  
+  height: 100vh;
+  width: 100%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// Element Plus
+import ElementPlus from 'element-plus';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import es from 'element-plus/es/locale/lang/es';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(ElementPlus, { locale: es });
+app.mount('#app');
